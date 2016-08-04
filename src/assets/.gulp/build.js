b75c371f2558e76ecd86d1934858c280c1a5a962
David Espinosa <davydeh@gmail.com>
2016-08-03 19:46:55 -0500

    updated build script #hotfix

--- src/assets/.gulp/build.js
+++ src/assets/.gulp/build.js
@@ -4,6 +4,7 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var zip  = require('gulp-zip');
+var sourcemaps = require('gulp-sourcemaps');
 
 var onError = function(err) {
   console.log(err.toString());
@@ -14,20 +15,20 @@ gulp.task('build:zip', function() {
 
   var distFiles = [
     '*.html',
-    '!index.html',
-    'assets/dist/**/*',
-
-    'assets/src/styles/**/*',
-
+   //  '!index.html',
+    'dist/**/*',
+    'src/assets/styles/**/*'
   ];
 
-  return gulp.src(distFiles, {base: '.'})
+  // return gulp.src('src/**/*')
+  return gulp.src(distFiles, { base: './' })
     .pipe(plumber({
       errorHandler: onError
     }))
-    .pipe(zip('latest.zip'))
+    .pipe(sourcemaps.init())
+      .pipe(zip('build.zip'))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('releases'));
 });
-
 
 gulp.task('build', ['default', 'build:zip']);

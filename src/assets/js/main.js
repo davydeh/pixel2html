db1ff7277ed37d1ab72afe7f736f26277825860e
David Espinosa <davydeh@gmail.com>
2016-08-05 11:50:08 -0500

    added stashed changes for hamburger menu and updated styles #fixes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -19,14 +19,37 @@ $(document).ready(function(){
       if(event.target.className === 'c-language__item') {
          $langModule.find('.c-language__select').html($this.text());
       }
-      
+
       $langModule.toggleClass('is-open');
    });
+
+   var toggleMenu = function() {
+      $(this).find('.c-hamburger').toggleClass('is-active');
+      $(this).find('.c-navigation').toggleClass('is-open');
+   }.bind(this);
+
+   var closeMenu = function() {
+      $(this).find('.c-hamburger').removeClass('is-active');
+      $(this).find('.c-navigation').removeClass('is-open');
+   }.bind(this);
 
    $(this).on('click', function(event){
       if(event.target.className !== 'c-language__select') {
          $('.c-language').removeClass('is-open');
       }
+
+      var $target = $(event.target);
+
+
+      // console.log(($target.hasClass('c-navigation') || $target.hasClass('c-hamburger') || $target.parents('.c-navigation, .c-hamburger').length > 0));
+      if(!($target.hasClass('c-navigation') || $target.hasClass('c-hamburger') || $target.parents('.c-navigation, .c-hamburger').length > 0)) {
+         closeMenu();
+      }
+
+   });
+
+   $('.c-hamburger').on('click', function(event){
+      toggleMenu();
    });
 
 });
